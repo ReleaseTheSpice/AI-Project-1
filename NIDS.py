603613679d0659fde3dbcc56203b1df1780528fc
Martin <mvanlaethem0@gmail.com>
2023-01-24 10:49:58 -0800

    nids file setup

---
+++ NIDS.py
@@ -0,0 +1,14 @@
+##############################################################
+### Martin Van Laethem  - Aiden Lonquist    - Cole Atkinson ###
+### A01189452           - A01189452         - A01189452     ###
+###  AI Project 1 - NIDS                                    ###
+###############################################################
+
+#from something import *
+
+class NIDS:
+    """class for the network intrusion detection system"""
+
+    def __init__(self, data):
+        self.data = data
+        self.data = self.data.split("
