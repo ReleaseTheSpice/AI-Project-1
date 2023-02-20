ed966567071c5625413d2d7ea433e3122225ac33
Martin <mvanlaethem0@gmail.com>
2023-02-19 18:52:54 -0800

    2

--- NIDS.py
+++ NIDS.py
@@ -15,6 +15,10 @@ from six import StringIO # is used for plotting the decision tree
 from IPython.display import Image # is used for plotting the decision tree
 from IPython.core.display import HTML # is used for showing the confusion matrix
 import pydotplus # is used for plotting the decision tree
+from sklearn.svm import SVR
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression
+import numpy
 # from correlation import uhhhh
 
 class NIDS:
@@ -47,7 +51,6 @@ class NIDS:
         data['dsport'] = pandas.to_numeric(data['dsport'], errors='coerce')
         data['dsport'] = data['dsport'].fillna(0)
 
-
         # Encode the source and destination IPs
         data['srcip'] = preprocessing.LabelEncoder().fit_transform(data['srcip'])
         data['dstip'] = preprocessing.LabelEncoder().fit_transform(data['dstip'])
@@ -58,43 +61,115 @@ class NIDS:
         data['state'] = pandas.factorize(data['state'])[0]
         data['service'] = pandas.factorize(data['service'])[0]
 
-        print(data.isnull().sum())
+        #data2 = featureAnalysis(data, featureCols)
+        rfe = recursiveFeatureElimination(data, featureCols)
+        #print(rfe.head())
 
-        # Make a new dataframe with only the rows that have an attack_cat (drop all rows where atkCat is null)
-        atkCatData = data
-        atkCatData.dropna(axis='rows', subset=['attack_cat'], inplace=True)
-
-
-        x = data[featureCols]                   # Features
-        yLabel = data['Label']                  # Target variable
-        xAtkCat = atkCatData[featureCols]       # Features
-        yAtkCat = atkCatData['attack_cat']      # Target variable
-
-        trainX, testX, trainY, testY = train_test_split(x, yLabel, test_size=0.2, random_state=1) # 80% training and 20% test
-        catTrainX, catTestX, catTrainY, catTestY = train_test_split(xAtkCat, yAtkCat, test_size=0.2,
-                                                        random_state=1)  # 80% training and 20% test
-
-        # Create Decision Tree classifer object
-        clf = DecisionTreeClassifier()
-        atkCatClf = DecisionTreeClassifier()
-
-        # # Train Decision Tree Classifer
-        clf = clf.fit(trainX, trainY)
-        atkCatClf = atkCatClf.fit(catTrainX, catTrainY)
-
-        #Predict the response for test dataset
-        prediction = clf.predict(testX)
-        atkCatPrediction = atkCatClf.predict(catTestX)
-
-        # Model Accuracy, how often is the classifier correct?
-        print("Accuracy:",metrics.accuracy_score(testY, prediction))
-        print(metrics.classification_report(testY, prediction))
-
-        # Model Accuracy, how often is the classifier correct?
-        print("CAT Accuracy:",metrics.accuracy_score(catTestY, atkCatPrediction))
-        print(metrics.classification_report(catTestY, atkCatPrediction))
+        classify(data, featureCols)
 
 
+def recursiveFeatureElimination(data, featureCols):
+    """Recursive feature elimination"""
+    x = data[featureCols]  # Features
+    y = data['Label']  # Target variable
+    nofList = numpy.arange(1, len(featureCols))
+    highScore = 0
+    scoreList = []
+    nof = 0
+
+    # for i in range(len(nofList)):
+    #     print("starting iteration " + str(i))
+    #     trainX, testX, trainY, testY = train_test_split(x, y, test_size=0.2,
+    #                                                     random_state=1)  # 80% training and 20% test
+    #     # Linear regression model?
+    #     model = LinearRegression()
+    #     # perform feature selection
+    #     rfe = RFE(model, n_features_to_select=nofList[i])
+    #     xTrainRfe = rfe.fit_transform(trainX, trainY)
+    #     xTestRfe = rfe.transform(testX)
+    #     # fit the model
+    #     model.fit(xTrainRfe, trainY)
+    #     score = model.score(xTestRfe, testY)
+    #     scoreList.append(score)
+    #     if (score > highScore):
+    #         highScore = score
+    #         nof = nofList[i]
+
+
+    #Linear regression model?
+    #model = LinearRegression()
+    model = SVR(kernel='linear')
+    # perform feature selection
+    rfe = RFE(model, n_features_to_select=43)
+    xRfe = rfe.fit_transform(x, y)
+    # fit the model
+    model.fit(xRfe, y)
+    print(rfe.support_)
+    print(rfe.ranking_)
+
+    # print("Optimal number of features: %d" % nof)
+    # print("Score with %d features: %f" % (nof, highScore))
+
+def featureAnalysis(data, featureCols):
+    """Analyze the features"""
+    # Create a new dataframe with only the features
+    data2 = data[featureCols]
+
+    # Create a new dataframe with only the features that have a correlation of 0.5 or higher
+    data3 = data2.corr()
+    data3 = data3[(data3 > 0.5) | (data3 < -0.5)]
+    data3 = data3.dropna(axis='columns', how='all')
+    data3 = data3.dropna(axis='index', how='all')
+    data3 = data3.dropna(axis='columns', how='all')
+    data3 = data3.dropna(axis='index', how='all')
+    print(data3)
+
+    # Create a new dataframe with only the features that have a correlation of 0.5 or higher
+    data4 = data2.corr()
+    data4 = data4[(data4 > 0.5) | (data4 < -0.5)]
+    data4 = data4.dropna(axis='columns', how='all')
+    data4 = data4.dropna(axis='index', how='all')
+    data4 = data4.dropna(axis='columns', how='all')
+    data4 = data4.dropna(axis='index', how='all')
+    print(data4)
+
+    return data2
+
+def classify(data, featureCols):
+    """Classify the data"""
+    # Make a new dataframe with only the rows that have an attack_cat (drop all rows where atkCat is null)
+    atkCatData = data
+    atkCatData.dropna(axis='rows', subset=['attack_cat'], inplace=True)
+
+    x = data[featureCols]  # Features
+    yLabel = data['Label']  # Target variable
+    xAtkCat = atkCatData[featureCols]  # Features
+    yAtkCat = atkCatData['attack_cat']  # Target variable
+
+    trainX, testX, trainY, testY = train_test_split(x, yLabel, test_size=0.2,
+                                                    random_state=1)  # 80% training and 20% test
+    catTrainX, catTestX, catTrainY, catTestY = train_test_split(xAtkCat, yAtkCat, test_size=0.2,
+                                                                random_state=1)  # 80% training and 20% test
+
+    # Create Decision Tree classifer object
+    clf = DecisionTreeClassifier()
+    atkCatClf = DecisionTreeClassifier()
+
+    # # Train Decision Tree Classifer
+    clf = clf.fit(trainX, trainY)
+    atkCatClf = atkCatClf.fit(catTrainX, catTrainY)
+
+    # Predict the response for test dataset
+    prediction = clf.predict(testX)
+    atkCatPrediction = atkCatClf.predict(catTestX)
+
+    # Model Accuracy, how often is the classifier correct?
+    print("Accuracy:", metrics.accuracy_score(testY, prediction))
+    print(metrics.classification_report(testY, prediction))
+
+    # Model Accuracy, how often is the classifier correct?
+    print("CAT Accuracy:", metrics.accuracy_score(catTestY, atkCatPrediction))
+    print(metrics.classification_report(catTestY, atkCatPrediction))
 
 if (__name__ == "__main__"):
     NIDS()
